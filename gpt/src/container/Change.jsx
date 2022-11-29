ce4fc94a48a3ed5620f2086cab9ac9656a20b728
ellrub <ellefsen88@gmail.com>
2022-11-29 13:00:25 +0100

    feature/sections
    added different sections

---
+++ gpt/src/container/Change.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Section } from '../components';
+
+
+
+const Change = () => {
+    return (
+    <Section>
+        <h2>"Equal opportunities"</h2>
+        <p><span>Be bold, be you.</span><br /> Girl Power Talk is aiming for equal opportinities 
+            not only for women, but also men and nonbinary individuals. We do this through meaningfull education so that
+            people can learn confidently and build a carrer full of purpose. We want to unlock your full potential, so that you
+            confidently can become a force for change.
+        </p>
+    </Section>
+)}
+
+export default Change;
---
+++ gpt/src/container/Leaders.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Section } from '../components';
+
+
+
+const Leaders = () => {
+    return (
+    <Section>
+        <h2>Become tomorrow's leader</h2>
+        <p><span>No bosses.</span><br /> At Girl Power Talk everyone is equal, this means that there are no bosses or managers.
+            Instead we have dedicated mentors that help guide our young leader. We encourage you to try, to fail and learn, 
+            and also to work accross departments. So if you dont want to be employee number thousand, but instead be part 
+            of something that is truly aiming to change the world, then Girl Power Talk is for you. Our young leaders comes from
+            all over the world, and if you share our state of mind, you can become one of them.
+
+        </p>
+    </Section>
+)}
+
+export default Leaders;
