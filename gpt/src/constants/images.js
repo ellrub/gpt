b7fd670683532d056697f1df9f6558a7de83feb8
ellrub <ellefsen88@gmail.com>
2022-11-29 13:00:37 +0100

    .

--- gpt/src/constants/images.js
+++ gpt/src/constants/images.js
@@ -1,5 +1,9 @@
 import logo from "../assets/gpt-logo.png"
+import logo_full from "../assets/gpt-logo-full.png"
+import logo_full_blue from "../assets/gpt-logo-full-blue.png"
 
 export default {
     logo,
+    logo_full,
+    logo_full_blue,
 };
