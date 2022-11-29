a48ceb0e37382780ca52dcbe097b2ff01cfc95ac
ellrub <ellefsen88@gmail.com>
2022-11-29 13:02:35 +0100

    fix/navbar

--- gpt/src/components/Navbar/Navbar.jsx
+++ gpt/src/components/Navbar/Navbar.jsx
@@ -9,11 +9,31 @@ import { images } from '../../constants';
 const Navbar = () => {
 const [toggle, setToggle] = useState(false)
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("logo__blue").style.opacity = "0";
+    document.getElementById("logo__white").style.opacity = "1";
+    document.getElementById("navbar").style.background = "#ff4c5b63";
+    document.getElementById("navbar").style['-webkit-backdrop-filter'] = 'blur(4px)';
+    document.getElementById("navbar").style['backdrop-filter'] = 'blur(4px)';
+    } else {
+    document.getElementById("logo__blue").style.opacity = "1";
+    document.getElementById("logo__white").style.opacity = "0";
+    document.getElementById("navbar").style.background = "none";
+    document.getElementById("navbar").style['-webkit-backdrop-filter'] = 'blur(0px)';
+    document.getElementById("navbar").style['backdrop-filter'] = 'blur(0px)';
+    }
+
+}
+
     return (
-    <nav className='app__navbar'>
+    <nav className='app__navbar' id='navbar'>
         <div className='app__navbar-logo'>
-            <img src={images.logo} alt="girl power talk logo"/>
-            <p><span>girl</span> power talk</p>
+            <img src={images.logo_full_blue} alt="girl power talk logo" id='logo__blue'/>
+            <img src={images.logo_full} alt="girl power talk logo" id='logo__white'/>
+            
         </div>
         <ul className='app__navbar-links'>
             {["apply now", "merch", "contact us"].map((item) => (
